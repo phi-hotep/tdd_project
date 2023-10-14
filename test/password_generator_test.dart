b373dfd991318148a4ff44d60e0d0f0f967006ba
phi-hotep <huguesarnold@gmail.com>
2023-10-14 11:56:30 +0100

    test written without Mockito package

--- test/password_generator_test.dart
+++ test/password_generator_test.dart
@@ -1,48 +1,44 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mockito/mockito.dart';
 import 'package:tdd_project/password_generator.dart';
-
-import 'test_helper.mocks.dart';
 
 void main() {
   late PassWord password;
-  late MockPassWord mockPassWord;
-  setUp(
-    () {
-      password = PassWord();
-      mockPassWord = MockPassWord();
-    },
-  );
+
+  setUp(() {
+    password = PassWord();
+  });
+
   group('genPwd', () {
     const String goodPassword = 'uTop58cNq';
 
     test(
       'doit retourner "Error" si le nombre entré est inférieur à 8',
       () async {
-        when(mockPassWord.genPwd(any)).thenReturn('Error');
-
+        // Act
         final result = password.genPwd(3);
 
+        // Assert
         expect(result, 'Error');
       },
     );
+
     test(
       'doit retourner un mot de passe dont la taille est égale au paramètre entré',
       () async {
-        when(mockPassWord.genPwd(any)).thenReturn(goodPassword);
-
+        // Act
         final result = password.genPwd(9);
 
+        // Assert
         expect(result.length, goodPassword.length);
       },
     );
     test(
       'doit contenir au moins une lettre en minuscule',
       () async {
-        when(mockPassWord.genPwd(any)).thenReturn(goodPassword);
-
+        // Act
         final result = password.genPwd(9);
 
+        // Assert
         expect(result, contains(RegExp('[a-z]')));
       },
     );
@@ -50,10 +46,10 @@ void main() {
     test(
       'doit contenir au moins une lettre en majuscule',
       () async {
-        when(mockPassWord.genPwd(any)).thenReturn(goodPassword);
-
+        // Act
         final result = password.genPwd(9);
 
+        // Assert
         expect(result, contains(RegExp('[A-Z]')));
       },
     );
@@ -61,10 +57,10 @@ void main() {
     test(
       'doit contenir au moins un chiffre',
       () async {
-        when(mockPassWord.genPwd(any)).thenReturn(goodPassword);
-
+        // Act
         final result = password.genPwd(9);
 
+        // Assert
         expect(result, contains(RegExp('[0-9]')));
       },
     );
