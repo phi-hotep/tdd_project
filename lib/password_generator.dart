6aa448c5ecf911d9a7292765b8a4976cf2097154
phi-hotep <huguesarnold@gmail.com>
2023-10-14 11:55:53 +0100

    function has been well refactorized

--- lib/password_generator.dart
+++ lib/password_generator.dart
@@ -2,16 +2,14 @@ import 'dart:math';
 
 class PassWord {
   String genPwd(int passwordLenght) {
-    if (passwordLenght < 8) {
-      return 'Error';
-    } else {
-      var charMin = 'abcdefghijklmnopqrstuvwxyz';
-      var charMaj = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-      var charNum = '0123456789';
-      var char = '$charMin$charMaj$charNum';
-      return List<dynamic>.generate(passwordLenght, (index) {
-        return char[Random.secure().nextInt(char.length)];
-      }).join('');
-    }
+    var charMin = 'abcdefghijklmnopqrstuvwxyz';
+    var charMaj = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    var charNum = '0123456789';
+    var char = '$charMin$charMaj$charNum';
+    String generatedPassword = List<dynamic>.generate(passwordLenght, (index) {
+      return char[Random.secure().nextInt(char.length)];
+    }).join('');
+
+    return (passwordLenght < 8) ? 'Error' : generatedPassword;
   }
 }
